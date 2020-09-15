0a23c5ff925b3ffe0da7e59da3804fbc74e4c98e
aozog <artur.ozog@gmail.com>
2020-09-15 15:12:37 +0200

    done2

--- zadania-domowe/js/9-funkcja-iloczyn/js/main.js
+++ zadania-domowe/js/9-funkcja-iloczyn/js/main.js
@@ -1,3 +1,5 @@
+// ES5
+
 function multiply(num1, num2, num3) {
     let result = num1 * num2 * num3;
     return result;
@@ -8,3 +10,25 @@ let multiplyResult = multiply(8, 3, 4);
 
 
 console.log(multiplyResult);
+
+
+
+
+// ES6
+
+// const multiply = (num1, num2, num3) => {
+//     let result = num1 * num2 * num3;
+//     return result;
+// }
+
+// let multiplyResult = multiply(8, 3, 4);
+// console.log(multiplyResult);
+
+
+
+
+
+// Skót
+// const multiply = (num1, num2, num3) => num1 * num2 * num3;
+// let multiplyResult = multiply(8, 3, 4);
+// console.log(multiplyResult);
